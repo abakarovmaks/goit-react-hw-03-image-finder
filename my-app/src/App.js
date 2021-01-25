ef3ac172a410f7edec9b2a9237b810eec2006274
abakarovmaks <abakarovmaks@gmail.com>
2021-01-25 15:53:06 +0200

    Add styles for ImageGallaryItem + add pictures

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,8 +1,8 @@
 import React, { Component } from 'react';
 import Container from './Components/Container/Container';
 import Searchbar from './Components/Searchbar/Searchbar';
-// import ImageGallery from './Components/ImageGallery/ImageGallery';
-// import Button from './Components/Button/Button';
+import ImageGallery from './Components/ImageGallery/ImageGallery';
+import Button from './Components/Button/Button';
 // import Loader from './Components/Loader/Loader';
 // import Modal from './Components/Modal/Modal';
 
@@ -16,10 +16,24 @@ export default class App extends Component {
     this.setState({ searchQuery: query });
   };
 
+  updateSearchPage = () => {
+    this.setState((prevState) => ({ searchPage: prevState.searchPage + 1 }));
+  };
+
+  resetSearchPage = () => {
+    this.setState({ searchPage: 1 });
+  };
+
   render() {
     return (
       <Container>
         <Searchbar getSearchQuery={this.getSearchQuery} />
+        <ImageGallery
+          searchQuery={this.searchQuery}
+          page={this.searchPage}
+          resetSearchPage={this.resetSearchPage}
+        />
+        <Button updateSearchPage={this.updateSearchPage} />
       </Container>
     );
   }
